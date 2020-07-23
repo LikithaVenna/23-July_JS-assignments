1f728426deeacdcb471b2e821255f1b4bf753f99
Likitha Raj <nagalikitha.venna@zensar.com>
2020-07-24 00:28:55 +0530

    Create js file to how immutable objects

--- findouts/immutable.js
+++ findouts/immutable.js
@@ -1,9 +1,32 @@
 // Immutable object (unchangeable object) is an object whose state cannot be modified after it is created.
 'use strict'
 
-var person = new ImmutableMap({name: "Chris", age: 32});
-var olderPerson = person.set("age", 33);
+console.log('----Using Freeze-----')
+ const book = Object.freeze({
+  title : "How JavaScript Works",
+  author : "Douglas Crockford"
+});
+// book.title = "Other title";
+console.log(book)
 
-person.toObject(); 
-olderPerson.toObject();
-console.log(person)
+console.log('----Using deepFreeze-----')
+function deepFreeze(object) {
+   Object.keys(object).forEach(function freezeNestedObjects(name){
+   const value = object[name];
+    if(typeof value === "object") {
+     deepFreeze(value);
+    }
+  });
+  return Object.freeze(object);
+}
+
+console.log('----Using map-----')
+
+const book2 = Map({
+  title: "JavaScript The Good Parts",
+  author: "Douglas Crockford"
+});
+const title = "How JavaScript Works"
+const newBook = book.set("title", title);
+console.log(newBook.toObject())
+ 
