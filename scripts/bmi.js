672e2fae375a7ba7540800596e0b098f064ac8d8
Likitha Raj <nagalikitha.venna@zensar.com>
2020-07-23 20:11:51 +0530

    Add terinary operators in this file

--- scripts/bmi.js
+++ scripts/bmi.js
@@ -6,5 +6,15 @@
      var res=(mass)/(height**2)
      return res
  }
- jeff = calculateBmi(5000,15)
+ jeff = calculateBmi(70,1.9)
  console.log(`BMI of jeff is ${jeff}`)
+ todd = calculateBmi(65,1.7)
+ console.log(`BMI of todd is ${todd}`)
+
+ // Comparing their BMI
+
+ let isHigh = (jeff > todd) ? "true" : "false"
+ 
+ // Display who has highest BMI
+
+ console.log(`It is ${isHigh} that jeff has high BMI than todd`)
