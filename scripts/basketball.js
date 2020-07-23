ffae1c9c4b74fbb257085fe4d66613d7575d5c70
Likitha Raj <nagalikitha.venna@zensar.com>
2020-07-23 20:29:17 +0530

    Create js file for scores html

---
+++ scripts/basketball.js
@@ -0,0 +1,23 @@
+'use strict'
+let Heath
+let Zane
+function score(s1,s2,s3){
+    var avg= (s1+s2+s3)/3
+}
+console.log('Display their average scores-----------')
+Heath= score(89,120,103)
+console.log(`Average score of Heath is ${Heath}`)
+Zane = score(116,94,123)
+console.log(`Average score of Heath is ${Zane}`)
+console.log('Display Winner-----------')
+if(avg1 > avg2){
+        console.log('Heath is the winner')
+    }else{
+         res=console.log('Zane is the winner')
+}
+// let final = winner(Heath,Zane)
+
+// console.log('Change their scores-----------')
+// Heath = score(100,130,111)
+// Zane = score(81,132,123)
+
