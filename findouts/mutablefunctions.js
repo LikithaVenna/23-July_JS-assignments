36616ebeca5d7bc7e3537635b809521b59ec0b8e
Likitha Raj <nagalikitha.venna@zensar.com>
2020-07-23 23:52:08 +0530

    Add mutable methods in js file

--- findouts/mutablefunctions.js
+++ findouts/mutablefunctions.js
@@ -2,11 +2,58 @@
 // In JavaScript, only objects and arrays are mutable, not primitive values.
 'use strict'
 
-var immutableString = "Hello";
+console.log('------using push and pop--------')
+let empInfo = ['Likitha','Zensar','Pune']
+empInfo.push('dotNet Batch')
+console.log(empInfo)
+console.log(empInfo.pop())
 
-// In the above code, a new object with string value is created.
 
-immutableString = immutableString + "World";
+console.log('------shift and unshift---')
+let mutatingRemove = ['a', 'b', 'c', 'd', 'e'];
+mutatingRemove.shift();
+console.log(mutatingRemove)
+let mutatingAdd = ['a', 'b', 'c', 'd', 'e']; 
+mutatingAdd.push('f'); 
+mutatingAdd.unshift('z');
+console.log(mutatingAdd)
 
-// We are now appending "World" to the existing value.
-console.log(immutableString)
+
+console.log('------Using splice---')
+mutatingRemove.splice(0, 2);
+console.log(mutatingRemove)
+
+console.log('------Using filter---')
+const arr1 = ['1', '2', '3', '4', '5'];
+const arr2 = arr1.filter(a => a !== '5');
+console.log(arr2)
+
+console.log('------using reference object--------')
+let a = {
+    foo: 'bar'
+};
+let b = a;
+b.foo =  'test2'
+console.log(b.foo); // test2
+console.log(a === b) // true
+
+console.log('------copy items---')
+var str1 = 'Hello';
+var num1 = 42;
+var num2 = num1;
+var str2 = str1;
+console.log(num2)
+
+console.log('------Update the copies---')
+num2 = num2 - 10;
+str2 += ' world';
+console.log(str2)
+
+
+
+
+
+
+
+
+
