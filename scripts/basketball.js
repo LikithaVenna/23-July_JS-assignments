55ca7b98507899c2d946aa000a4f386a9c8c424c
Likitha Raj <nagalikitha.venna@zensar.com>
2020-07-23 20:52:07 +0530

    Add loops and if statemts in js file

--- scripts/basketball.js
+++ scripts/basketball.js
@@ -1,23 +1,44 @@
 'use strict'
 let Heath
 let Zane
+let score1 
+let score2
 function score(s1,s2,s3){
     var avg= (s1+s2+s3)/3
+    return avg
 }
-console.log('Display their average scores-----------')
-Heath= score(89,120,103)
-console.log(`Average score of Heath is ${Heath}`)
-Zane = score(116,94,123)
-console.log(`Average score of Heath is ${Zane}`)
-console.log('Display Winner-----------')
+console.log('-----------Display their average scores-----------')
+score1= score(89,120,103)
+console.log(`Average score of Heath is ${score1}`)
+score2 = score(116,94,123)
+console.log(`Average score of Heath is ${score2}`)
+console.log('---------Display Winner-----------')
+function winner(avg1,avg2){
+    var res
 if(avg1 > avg2){
-        console.log('Heath is the winner')
+        res=console.log('Heath is the winner')
     }else{
-         res=console.log('Zane is the winner')
+        res=console.log('Zane is the winner')
 }
-// let final = winner(Heath,Zane)
+return res
+}
+let round1 = winner(score1,score2)
+console.log('---------Change their scores-----------')
+let score3 = score(100,130,111)
+let score4 = score(81,132,123)
+let round2 = winner(score3,score4)
+console.log('---------Add player Mariah-----------')
 
-// console.log('Change their scores-----------')
-// Heath = score(100,130,111)
-// Zane = score(81,132,123)
+let score5 = score(97,134,105)
+
+console.log(`Mariah score is ${score5}`)
+if((score5 > score1) && (score5 > score2))
+{
+    console.log('Mariah is the highest')
+
+}else{
+    console.log('Mariah is not highest')
+}
+
+
 
